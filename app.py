4ab94979e969839334f460d027f52a2f16005ca3
Sachin-nb <sachinn@kssem.edu.in>
2026-03-25 10:21:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -951,5 +951,5 @@ def delete_post(post_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000)
 
